455a661502c8332ed9501a869db51be433ef0edb
GilbertoManunta <gilberto.manunta@gmail.com>
2018-01-21 22:07:22 +0100

    Add files via upload

--- typingg24.ml
+++ typingg24.ml
@@ -57,9 +57,33 @@ TVar ("?T" ^ string_of_int (!nextsym));;
 
 exception TypeExc of string;;
 
+let newtypenv = [(Ide "",Tchar)];;
+
+let rec applytypenv a i = match (a,i) with
+  (Ide s,TBool)::tl,(Ide s1) -> if s1 = s then TBool else applytypenv tl i
+| (Ide s,Tchar)::tl,(Ide s1) -> if s1 = s then Tchar else applytypenv tl i
+| (Ide s,TInt)::tl,(Ide s1) ->  if s1 = s then TInt else applytypenv tl i
+| (Ide s,TVar st)::tl,(Ide s1) -> if s1 = s then TVar st else applytypenv tl i
+| (Ide s,TPair(e1,e2))::tl,(Ide s1) -> if s1 = s then TPair(e1,e2) else applytypenv tl i
+| (Ide s,TList l)::tl,(Ide s1) -> if s1 = s then TList l else applytypenv tl i
+| (Ide s,TFun(x,t))::tl,(Ide s1) -> if s1 = s then TFun(x,t) else applytypenv tl i
+| [],_ -> failwith "ambiente tipi vuoto";;
+
+let bindtyp env id tipo = match(id,tipo) with
+   (Ide s,e) when e!=tipo-> (id,tipo)::env
+| (Ide s,TBool)-> (id,TBool)::env
+| (Ide s,Tchar)-> (id,Tchar)::env
+| (Ide s,TInt)-> (id,TInt)::env
+| (Ide s,TVar st)-> (id,TVar st)::env
+| (Ide s,TPair(e1,e2))-> (id,TPair(e1,e2))::env
+| (Ide s,TList l)-> (id,TList l)::env
+| (Ide s,TFun(x,t))-> (id,TFun(x,t))::env;;
+
+let env = newtypenv;;
+
 let rec typeinf e = match e with
 |Val(i)-> (match i with
-    Ide s-> TVar s
+    Ide s-> (applytypenv env i)
   |_->raise (TypeExc ("Not a ide")))
 |Eint(e) -> TInt
 |Echar(e) -> Tchar
@@ -94,7 +118,7 @@ let rec typeinf e = match e with
   raise (TypeExc ("Not a TList"))
 |Tail(e) -> if typeinf e = TList ((typeinf e)::[]) then TList((typeinf e)::[]) else
   raise (TypeExc ("Not a TList"))
-|Fst(e)->( match (typeinf e) with
+|Fst(e)-> (match (typeinf e) with
    TPair(ex1,ex2)-> ex1
   | _ -> raise (TypeExc ("Not a TPair")))
 |Snd(e) -> (match (typeinf e) with
@@ -106,16 +130,12 @@ let rec typeinf e = match e with
       typeinf e1 else
       raise (TypeExc ("Different types")) else
     raise (TypeExc ("Not a TBool"))
-|Fun(x,t)-> (match (x,typeinf t) with
-    (Ide s,e) -> TFun(newvar(),e)
-  |(_,_)->raise(TypeExc("Not a ide")))
-|Appl(e1,e2)->let tipo=typeinf e2 and a=newvar() in
-  if(typeinf e1=TFun(tipo,a)) then a
-  else raise(TypeExc ("Not applicable"))
-|Let(x,t1,t2)-> typeinf t2
-;;
+|Fun(x,t)-> let a = newvar() in
+  bindtyp env x a;
+    (match (x,typeinf t) with
+    (Ide s,e) -> TFun(a,e));;
   
-typeinf e5;;
+typeinf e1;;
 
 let e = Fun(Ide "x",Ifthenelse(Eq(Val(Ide "x"),Empty),True,False));;
 let e1=Fun(Ide "x",Fun(Ide "y",Val(Ide "x")));;
@@ -124,3 +144,8 @@ let e3 =Cons(Eint 1, Cons(Eint 2, Empty));;
 let e4=Cons(Cons(Eint 1,Empty),Empty);;
 let e5=Let(Ide "f", Fun(Ide "x",Fun(Ide "y",Val(Ide "x"))),
 	   Appl(Appl(Val(Ide "f"),Eint 2),Eint 1));;
+let e6 = Cons(Empty,Empty);;
+let e7 = Fun(Ide "x", Ifthenelse(Eq(Val(Ide "x"),Empty),True,False));;
+let e8 = Epair(Fun(Ide "x", Ifthenelse(Eq(Val(Ide "x"),Empty),True,False)),
+      Cons(Cons(Eint 1,Empty),Empty));
+let e9 = Val(Ide "x");;
